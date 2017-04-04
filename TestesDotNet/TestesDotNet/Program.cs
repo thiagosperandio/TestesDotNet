7d1acdd7bf3b425b4bc22ff7fe8449efb4890e72
ThiagoSperandio <thiagosperandio@gmail.com>
2017-04-04 17:58:03 -0300

    chore(Solution): Criando projeto Console Application no VS
    Criando projeto para realizar os testes na aplicação.

---
+++ TestesDotNet/TestesDotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
